42c8b8066008315c3327fea687ecfefed5ecf05a
Detlef von der Thuesen <detlef.vonderthuesen@holisticon.de>
2017-11-13 18:49:37 +0100

    reconstruction with remote accepting current changes

--- src/app/forms/simple-reactive-form/simple-reactive-form.component.ts
+++ src/app/forms/simple-reactive-form/simple-reactive-form.component.ts
@@ -1,10 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 import { CustomInputComponent } from '../../custom-input/custom-input.component';
-<<<<<<< HEAD
-=======
-
->>>>>>> 8909737d6d58a14f3f4af368c6709c4bf67cff29
 // ngx-translate
 import { TranslateService } from '@ngx-translate/core';
 
@@ -12,22 +8,18 @@ import { TranslateService } from '@ngx-translate/core';
   // tslint:disable-next-line:component-selector
   selector: 'app-simple-reactive-form',
   templateUrl: './simple-reactive-form.component.html',
-<<<<<<< HEAD
   styleUrls: ['./simple-reactive-form.component.scss'],
-=======
-  styleUrls: ['./simple-reactive-form.component.css'],
->>>>>>> 8909737d6d58a14f3f4af368c6709c4bf67cff29
   encapsulation: ViewEncapsulation.None
 })
 export class SimpleReactiveFormComponent implements OnInit {
 
-<<<<<<< HEAD
+  /**
+   * TODO: implement custom input element
+   */
+
   constructor(private translate: TranslateService) {
     translate.setDefaultLang('de');
   }
-=======
-  constructor() { }
->>>>>>> 8909737d6d58a14f3f4af368c6709c4bf67cff29
 
   ngOnInit() {
   }
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { SimpleReactiveFormComponent } from './forms/simple-reactive-form/simple-reactive-form.component';
@@ -7,12 +6,4 @@ export const ROUTES = [
   { path: 'simpleform', component: SimpleReactiveFormComponent },
   { path: 'home', component: HomeComponent },
   { path: '', pathMatch: 'full', redirectTo: 'home' },
-=======
-import { SimpleReactiveFormComponent } from './forms/simple-reactive-form/simple-reactive-form.component';
-import { AppComponent } from './app.component';
-
-export const ROUTES = [
-  { path: 'simpleform', component: SimpleReactiveFormComponent },
-  { path: '', component: AppComponent },
->>>>>>> 8909737d6d58a14f3f4af368c6709c4bf67cff29
 ];
